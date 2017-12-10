791c7fdca3a4ef3365fe828fca8c0e8973159dae
Ajay Narain Mathur <ajaynarainmathur@gmail.com>
2017-12-10 15:16:56 +1100

    updated comments 🙃

--- index.js
+++ index.js
@@ -5,15 +5,19 @@ module.exports = function({ types: t }) {
     visitor: {
       ClassMethod(path) {
         const { node } = path;
+        // see if method is set or get
         if (["set", "get"].indexOf(node.kind) > -1) {
+          // check if it is single child of the container
           if (path.inList && path.container && path.container.length === 1) {
             let CLASS_NAME;
+            // get the class name on parent of path
             path.findParent(path => {
               if (!path.isClassDeclaration()) {
                 return;
               }
               CLASS_NAME = path.node.id.name;
             });
+            // mutate path
             path.replaceWith(
               buildRequire({
                 KIND: t.identifier(node.kind),
@@ -29,6 +33,7 @@ module.exports = function({ types: t }) {
   };
 };
 
+// template for transformation
 const buildRequire = template(`
   Object.defineProperty(CLASS_NAME, KEY, {
     KIND: FUNCTION_BODY,
